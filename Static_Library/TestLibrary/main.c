7b494ff3fa1af8e90dfbf2ec7abdd4b567d95971
Auresol69 <quocbao962005@gmail.com>
2025-08-05 14:36:48 +0700

    Static Library

---
+++ Static_Library/TestLibrary/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "mylib.h"
+
+int main(void)
+{
+    fun();
+    return 0U;
+}
---
+++ Static_Library/mylib/Sources/mylib.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "mylib.h"
+
+void fun(void)
+{
+    printf("my static library\n");
+}
