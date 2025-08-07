a83c9097d8308f7be2df681ebcfce83e13c1cdcb
Auresol69 <quocbao962005@gmail.com>
2025-08-07 16:18:57 +0700

    gcc and Dynamic Library

---
+++ Dynamic_Library/Testlib/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "mylib.h"
+
+int main(void)
+{
+    fun();
+    return 0U;
+}
---
+++ gcc/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define x 1
+
+int main(void)
+{
+    int a = x;
+    printf("a = %d\n", a);
+    return 0U;
+}
